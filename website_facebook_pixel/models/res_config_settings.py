9d0525612bf832b9e1247f9f0028c26bcbfcabf1
Hernan Rocha <hernanrocha93@gmail.com>
2021-05-30 22:45:29 -0300

    [website_facebook_pixel] ]Add domain verification code

--- website_facebook_pixel/models/res_config_settings.py
+++ website_facebook_pixel/models/res_config_settings.py
@@ -11,6 +11,11 @@ class ResConfigSettings(models.TransientModel):
         readonly=False,
     )
 
+    facebook_domain_verification_code = fields.Char(
+        related='website_id.facebook_domain_verification_code',
+        readonly=False,
+    )
+
     @api.depends('website_id')
     def has_facebook_pixel(self):
         self.has_facebook_pixel = bool(self.facebook_pixel_key)
@@ -18,6 +23,7 @@ class ResConfigSettings(models.TransientModel):
     def inverse_has_facebook_pixel(self):
         if not self.has_facebook_pixel:
             self.facebook_pixel_key = False
+            self.facebook_domain_verification_code = False
 
     has_facebook_pixel = fields.Boolean(
         string='Facebook Pixel',
--- website_facebook_pixel/models/website.py
+++ website_facebook_pixel/models/website.py
@@ -7,3 +7,4 @@ class Website(models.Model):
     _inherit = 'website'
 
     facebook_pixel_key = fields.Char('Facebook Pixel ID')
+    facebook_domain_verification_code = fields.Char('Facebook Domain Verification Code')
