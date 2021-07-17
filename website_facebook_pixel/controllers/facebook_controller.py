9c01a67ea02f7f8d01108a161cbc40e4e8c8ba14
Hernan Rocha <hernanrocha93@gmail.com>
2021-07-17 17:07:28 -0300

    Add http validation

---
+++ website_facebook_pixel/controllers/facebook_controller.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo import http
+
+class FacebookController(http.Controller):
+    @http.route('/a0cs7llpltithpuajkvdq9cevd3nqp.html', type='http', auth='user')
+    def handler(self):
+        return "a0cs7llpltithpuajkvdq9cevd3nqp"
