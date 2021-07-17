b8d0ffc6251ad2f7c34ff15acfe2f73689644668
Hernan Rocha <hernanrocha93@gmail.com>
2021-07-17 16:39:36 -0300

    Revert "[website_facebook_pixel] ]Add domain verification code"
    This reverts commit 9d0525612bf832b9e1247f9f0028c26bcbfcabf1.

--- website_facebook_pixel/models/res_config_settings.py
+++ website_facebook_pixel/models/res_config_settings.py
@@ -11,11 +11,6 @@ class ResConfigSettings(models.TransientModel):
         readonly=False,
     )
 
-    facebook_domain_verification_code = fields.Char(
-        related='website_id.facebook_domain_verification_code',
-        readonly=False,
-    )
-
     @api.depends('website_id')
     def has_facebook_pixel(self):
         self.has_facebook_pixel = bool(self.facebook_pixel_key)
@@ -23,7 +18,6 @@ class ResConfigSettings(models.TransientModel):
     def inverse_has_facebook_pixel(self):
         if not self.has_facebook_pixel:
             self.facebook_pixel_key = False
-            self.facebook_domain_verification_code = False
 
     has_facebook_pixel = fields.Boolean(
         string='Facebook Pixel',
